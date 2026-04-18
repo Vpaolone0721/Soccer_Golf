d5600bbcc3bc458646104d417437c65e167d97dd
Vpaolone0721 <Vpaolone0721@gmail.com>
2026-04-18 00:07:54 -0400

    fourth commit

--- start_screen.gd
+++ start_screen.gd
@@ -1,8 +1,47 @@
 extends Control
+@onready var main_page = $"Panel/Main Screen"
+@onready var controls_page = $"Panel/Controls Screen"
+@onready var levels_page = $"Panel/Levels"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	controls_page.visible = false
+	controls_page.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	levels_page.visible = false
+	levels_page.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
-func _on_label_pressed() -> void:
+func _on_play_pressed() -> void:
+	main_page.visible = false
+	levels_page.visible = true
+	levels_page.mouse_filter = Control.MOUSE_FILTER_STOP
+	main_page.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
+func _on_controls_pressed() -> void:
+	main_page.visible = false
+	controls_page.visible = true
+	main_page.mouse_filter = Control.MOUSE_FILTER_STOP
+	controls_page.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
+func _on_back_pressed() -> void:
+	main_page.visible = true
+	controls_page.visible = false
+	main_page.mouse_filter = Control.MOUSE_FILTER_STOP
+	controls_page.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
+func _on_back_levels_pressed() -> void:
+	main_page.visible = true
+	levels_page.visible = false
+	main_page.mouse_filter = Control.MOUSE_FILTER_STOP
+	levels_page.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+func level_1() -> void:
+	get_tree().change_scene_to_file("res://level_1.tscn")
+	
+func level_2() -> void:
+	get_tree().change_scene_to_file("res://level_2.tscn")
+	
+func level_3() -> void:
 	get_tree().change_scene_to_file("res://level_3.tscn")
+
+
+#get_tree().change_scene_to_file("res://level_3.tscn")
